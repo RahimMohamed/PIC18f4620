5045a492ffb3e54aad8e94a44a6f5887670cbd92
RahimMohamed <111053544+RahimMohamed@users.noreply.github.com>
2024-01-25 22:57:57 +0200

    add ECU & MACL

---
+++ ECU_LAYER/RELAY/ecu_relay_cfg.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   cfg_relay.h
+ * Author: gaming3
+ *
+ * Created on November 3, 2023, 2:44 AM
+ */
+
+#ifndef CFG_RELAY_H
+#define	CFG_RELAY_H
+
+
+/*section : Includes  */
+
+/*section : Macro Declaration  */
+
+
+/*section : Macro Functions Declaration  */
+
+
+/*section : Data Type Declaration  */
+
+
+/*section : Macro Declaration  */
+
+
+/*section :  Functions Declaration  */
+
+#endif	/* CFG_RELAY_H */
+
